2ad1e592fc25c3773f41c1e510bedbc54b3aa85b
SanjayAmirneni <sanjayamirneni@gmail.com>
2022-06-20 00:04:15 -0500

    06/20/2022

---
+++ pascalTriangle.js
@@ -0,0 +1,18 @@
+function pascalTraingle(rowCount){
+    let traingle = [];
+    traingle.push([1]);
+    for(let i=1; i<rowCount; i++){
+        let prevArr = traingle[i-1];
+        let arr = [];
+        arr.push(1);
+        for(let j=1; j<i; j++){
+            arr.push(prevArr[j-1]+prevArr[j]);
+        }
+        arr.push(1);
+        traingle.push(arr);
+    }
+
+    return traingle;
+}
+
+console.log(pascalTraingle(10));
